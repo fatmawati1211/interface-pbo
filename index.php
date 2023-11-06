38d726120e163aba932a6458c410121d788fac89
farhanrfz <farhanreza09@gmail.com>
2023-11-06 10:47:16 +0700

    index

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+include_once "Komputer.php";
+include_once "Laptop.php";
+
+$komputer= new Komputer();
+
+echo $komputer->hidupkan();
+echo "<br/>";
+echo $komputer->matikan();
+echo "<br/>";
+
+$laptop = new Laptop();
+
+echo $laptop->hidupkan();
+echo "<br/>";
+echo $laptop->matikan();
+echo "<br/>";
+?>
